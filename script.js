411fd129dd27236248c3a5c29853edd37ac5d89e
Pranshu Lokhande <89801248+pranshuuuu@users.noreply.github.com>
2022-09-24 23:48:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+$(document).ready(function(){
+
+  $('#menu').click(function(){
+    $(this).toggleClass('fa-times');
+    $('header').toggleClass('toggle');
+  });
+
+  $(window).on('scroll load',function(){
+
+    $('#menu').removeClass('fa-times');
+    $('header').removeClass('toggle');
+
+    if($(window).scrollTop() > 0){
+      $('.top').show();
+    }else{
+      $('.top').hide();
+    }
+
+  });
+
+  // smooth scrolling 
+
+  $('a[href*="#"]').on('click',function(e){
+
+    e.preventDefault();
+
+    $('html, body').animate({
+
+      scrollTop : $($(this).attr('href')).offset().top,
+
+    },
+      500, 
+      'linear'
+    );
+
+  });
+
+});
+
+/////
+var typed = new Typed(".typing",{
+  strings:["Wide Variety Of opportunities","Certificate-Free Courses","Helps to improve your Skills","Provides friendly eco-system of innovators,designers","Join Now"],
+  typeSpeed:100,
+  backspeed:60,
+  loop: true
+})
